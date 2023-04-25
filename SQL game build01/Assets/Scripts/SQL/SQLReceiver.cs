fd9038d14ba2daa202a884e1ae0991f1bbf0c9cf
salaza <gun159@outlook.com>
2023-04-25 21:59:06 +0700

    make method for check permission sql command

--- SQL game build01/Assets/Scripts/SQL/SQLReceiver.cs
+++ SQL game build01/Assets/Scripts/SQL/SQLReceiver.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
+using System;
 
 public class SQLReceiver : MonoBehaviour
 {
+    private string[] bannedWord = {"create", "update", "delete", "insert", "drop", "alter", "truncate", "grant", "revoke", "commit", "rollback", "savepoint"};
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +17,21 @@ public class SQLReceiver : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public bool haveBannedWord(string sqlCommand)
+    {
+        string[] sqlWords = sqlCommand.ToLower().Split(' ');
+
+        foreach(string word in sqlWords)
+        {
+            if (bannedWord.Contains(word))
+            {
+                Debug.Log("You don't have permission to use this command: \"Create\", \"Insert\", \"Update\", \"Delete\"");
+                return true;
+            }
+        }
+
+        return false;
     }
 }
