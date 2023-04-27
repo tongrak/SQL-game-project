eeb9aad76c359a108f876e724e1b8127616b7fbe
salaza <gun159@outlook.com>
2023-04-27 21:45:48 +0700

    init SQLChecker class

---
+++ SQL game build01/Assets/Scripts/SQL/SQLChecker.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SQLChecker
+{
+    public SQLChecker()
+    {
+
+    }
+
+    public bool CheckAnswer(string pQuery, string anQuery)
+    {
+        return true;
+    }
+
+    public string GetPlayerResult()
+    {
+        return "";
+    }
+
+    public string GetAnswerResult()
+    {
+        return "";
+    }
+}
