fd306201264c598f672f1c94f99c4c69ff812efa
salaza <gun159@outlook.com>
2023-04-28 11:27:57 +0700

    catch error from query

--- SQL game build01/Assets/Scripts/SQL/SQLChecker.cs
+++ SQL game build01/Assets/Scripts/SQL/SQLChecker.cs
@@ -1,26 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
+using Mono.Data.Sqlite; // 1
+using System.Data; // 1
 using UnityEngine;
 
 public class SQLChecker
 {
-    public SQLChecker()
-    {
+    private string dbPath;
 
+    public SQLChecker(string dbPath)
+    {
+        this.dbPath = dbPath;
     }
 
     public bool CheckAnswer(string pQuery, string anQuery)
     {
+        //return GetQueryResult(pQuery).Equals(GetQueryResult(anQuery));
         return true;
     }
 
-    public string GetPlayerResult()
+    //public string GetPlayerResult(string pQuery)
+    //{
+    //    return "";
+    //}
+
+    //public string GetAnswerResult(string anQuery)
+    //{
+    //    return "";
+    //}
+
+    // return result from query in json form.
+    public string GetQueryResult(string query)
     {
+        //string result;
+        try
+        {
+            // Connect to database
+            using (SqliteConnection connection = new SqliteConnection(dbPath))
+            {
+                connection.Open();
+                // Query to database
+                using (SqliteCommand command = new SqliteCommand(query, connection))
+                {
+                    // Read data from query
+                    using (IDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Debug.Log(reader.GetValue(1).ToString());
+                        }
+                    }
+                }
+                connection.Close();
+            }
+        }
+        catch (SqliteException e)
+        {
+            Debug.Log(e.Message);
+            return e.Message;
+        }
         return "";
     }
 
-    public string GetAnswerResult()
-    {
-        return "";
-    }
 }
---
+++ SQL game build01/Assets/Tests/EditMode/SQLCheckerTests.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class SQLCheckerTests
+{
+    // A Test behaves as an ordinary method
+    [Test]
+    public void SQLCheckerTestsSimplePasses()
+    {
+        string path = "URI=file:" + Application.dataPath + "/Database/DemoDatabase.db";
+        string query = "SELECT";
+        SQLChecker sqlCh = new SQLChecker(path);
+
+        Assert.AreEqual("", sqlCh.GetQueryResult(query));
+    }
+}
