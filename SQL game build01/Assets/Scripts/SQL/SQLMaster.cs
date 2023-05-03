271f5dec5e49720da031b8bedcad287045b61491
salaza <gun159@outlook.com>
2023-05-03 21:46:28 +0700

    create function for get result from querying approximate

--- SQL game build01/Assets/Scripts/SQL/SQLMaster.cs
+++ SQL game build01/Assets/Scripts/SQL/SQLMaster.cs
@@ -2,17 +2,49 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SQLMaster : MonoBehaviour
+interface SQLMasterInt
 {
+    void GetResult(string pQuery, string anQuery);
+}
+
+public class SQLMaster : MonoBehaviour, SQLMasterInt
+{
+    //[SerializeField] PuzzleMaster puzzleMas;
+
+    private SQLChecker checker;
+    private SQLReceiver receiver;
+    private string _dbPath;
+    //private string _dbPath = puzzleMas.Get_dbPath;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(receiver == null)
+        {
+            receiver = new SQLReceiver();
+        }
+        if(checker == null)
+        {
+            checker = new SQLChecker(_dbPath);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void GetResult(string pQuery, string anQuery)
+    {
+        if (receiver.haveBannedWord(pQuery))
+        {
+            Debug.Log("Player query is not valid.");
+        }
+        else
+        {
+            SQLResult result;
+            result = checker.CheckAnswer(pQuery, anQuery);
+        }
     }
 }
