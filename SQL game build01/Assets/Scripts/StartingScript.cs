af3f3b4e65aba06509c80b6652b8bdc5ade057fc
Oliver Tongrak <oliver.tongrak@gmail.com>
2023-05-07 15:29:27 +0700

    Starting Scene added
    Added start to load additive scene

--- SQL game build01/Assets/Scripts/StartingScript.cs
+++ SQL game build01/Assets/Scripts/StartingScript.cs
@@ -1,18 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class StartingScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //A list of scene to load before the room is self. Like Consoles, Player?, Music?, other.
+    [SerializeField] private string[] _passiveGameplayScenes;
+
+    private GameObject _mastersObj;
+    private string _startingScene;
+
     void Start()
     {
-        
-    }
+        _mastersObj = GameObject.Find("Masters");
+        _startingScene = this.gameObject.scene.name;
 
-    // Update is called once per frame
-    void Update()
+        //Put Masters object to DontDestroyOnLoad Scene.
+        if (_mastersObj != null) DontDestroyOnLoad(_mastersObj);
+        else throw new MissingComponentException("Masters Object not detected");
+
+        //Loading every passive gameplay scenes.
+        foreach (var s in _passiveGameplayScenes)
+        {
+            try
+            {
+                SceneManager.LoadSceneAsync(s, LoadSceneMode.Additive);
+            }
+            catch(System.Exception e) { Debug.LogWarning("Starting scene err: " + e); }
+        }
+        //Unloading starting scene
+        SceneManager.sceneLoaded += ActivatorAndUnloader;
+    }
+    
+    //Still do not know how it work...
+    void ActivatorAndUnloader(Scene s, LoadSceneMode m)
     {
-        
+        SceneManager.sceneLoaded -= ActivatorAndUnloader;
+        SceneManager.SetActiveScene(s);
+        SceneManager.UnloadSceneAsync(_startingScene);
     }
 }
