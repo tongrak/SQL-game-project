20f3a7fbaf8a5eb251f4deec87aad22662cdf2e2
Oliver Tongrak <oliver.tongrak@gmail.com>
2023-05-13 12:38:35 +0700

    Execute Buffering Time added

--- SQL game build01/Assets/Scripts/Console Scripts/Puzzle console/PuzzleConsoleMaster.cs
+++ SQL game build01/Assets/Scripts/Console Scripts/Puzzle console/PuzzleConsoleMaster.cs
@@ -2,15 +2,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace ConsoleGeneral
 {
     public class PuzzleConsoleMaster : ConsoleBasic
     {
+        //For raising execution call
         public event ExcuteButtonHandler ExcutionCalled;
 
+        [Header("Displaying Element")]
+        [SerializeField] private Button _buttonElement;
+
+        //Input box feilds;
         private string _currOutputString;
         private string _currInputString;
+
+        //Config fields
+        private bool _canExecute = true;
+        [Header("Configure variable")]
+        [SerializeField] private int _exeBuffer = 1;
 
         #region Input Box
         public void UpdateCurrInput(string s)
@@ -20,10 +31,23 @@ namespace ConsoleGeneral
         #endregion
 
         #region Excute Butt
+        IEnumerator ExecutionBuffer(int sec)
+        {
+            yield return new WaitForSeconds(sec);
+            _canExecute = true;
+            _buttonElement.interactable = true;
+        }
+
         public virtual void ExcutionButtonAct()
         {
             _currOutputString = _currInputString;
-            ExcutionCalled?.Invoke(_currOutputString);
+            if (_canExecute)
+            {
+                _canExecute = false;
+                _buttonElement.interactable = false;
+                ExcutionCalled?.Invoke(_currOutputString);
+                StartCoroutine(ExecutionBuffer(_exeBuffer));
+            }
         }
         #endregion
     }
