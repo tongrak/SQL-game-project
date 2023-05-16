29bc5b5622ba44588ba3e28cc6c8103709cf78ca
Oliver Tongrak <oliver.tongrak@gmail.com>
2023-05-16 15:33:24 +0700

    Chapter initialization feature added
    implement and briefly test.

--- SQL game build01/Assets/Scripts/Masters/ChaptersMaster.cs
+++ SQL game build01/Assets/Scripts/Masters/ChaptersMaster.cs
@@ -1,5 +1,7 @@
+using GluonGui.WorkspaceWindow.Views.WorkspaceExplorer;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -10,12 +12,52 @@ namespace ChapNRoom
 {
     public class ChaptersMaster : MonoBehaviour
     {
+        [SerializeField] private string _chapterFolderName = "Chapters";
 
-        //private ChapInterpreterInter _ChapI;
-        
+        private ChapterInterpreter _ChapI = ChapterInterpreter.Instance;
+
+        //Config var
+        private string _defaultDirPath;
+        private string[] _chapterRefPaths;
+        private ChapterRef[] _chapterRefs;
+
+        private string[] GetChapterFileFrom(string chapterRefsPath)
+        {
+            //try getting all txt files from given path.
+            return Directory.GetFiles(chapterRefsPath, "*.txt");
+
+        }
+
+        private ChapterRef[] GetChapRefFromPaths(string[] chapRefPaths)
+        {
+            List<ChapterRef> chaptersBuffer = new List<ChapterRef>();
+
+            foreach (string path in chapRefPaths)
+            {
+                try
+                {
+                    chaptersBuffer.Add(_ChapI.GetChapRefFrom(path));
+                }
+                catch (System.Exception ex)
+                {
+                    Debug.LogWarning("Cann't add chapter from :" +  path + " ; Due to: " + ex.ToString()); 
+                }
+            }
+
+            return chaptersBuffer.ToArray();
+        }
+
+        //private void 
 
         void Start()
         {
+            //Init var
+            _defaultDirPath = string.Format(Application.dataPath+@"\{0}",_chapterFolderName);
+            _chapterRefPaths = GetChapterFileFrom(_defaultDirPath);
+            _chapterRefs = GetChapRefFromPaths(_chapterRefPaths);
+
+           
+
             //Dummy line
             SceneManager.LoadSceneAsync(2, LoadSceneMode.Additive);
         }
