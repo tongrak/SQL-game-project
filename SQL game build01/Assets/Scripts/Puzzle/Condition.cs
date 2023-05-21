dc25fd598102abe0a4fb30d34f5e1af4f5fd48f7
salaza <gun159@outlook.com>
2023-05-21 10:35:28 +0700

    Create class for collect condition in from json

---
+++ SQL game build01/Assets/Scripts/Puzzle/Condition.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+
+namespace conditionModel
+{
+    public class Condition
+    {
+        public int joinNum;
+        public bool join;
+        public int nestedNum;
+        public int executeNum;
+    }
+}
