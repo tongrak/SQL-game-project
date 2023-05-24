0c056a8a839eb14d6abd4fa3ae603bde31f5781f
salaza <gun159@outlook.com>
2023-05-24 18:30:34 +0700

    GetCondMessage method in Condition class

--- SQL game build01/Assets/Scripts/Puzzle/Condition.cs
+++ SQL game build01/Assets/Scripts/Puzzle/Condition.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 
@@ -11,5 +12,93 @@ namespace PuzzleController
         public string nestedNum;
         public string executeNum;
         public string whereCondNum;
+
+        public string[] GetConditionMessage()
+        {
+            List<string> condMessage = new List<string>();
+
+            condMessage.Add("Correct the query");
+            if (!joinNum.Equals(null))
+            {
+                condMessage.Add(GetjoinNumMessage());
+            }
+            if (!haveJoin.Equals(null))
+            {
+                condMessage.Add(GetHaveJoinMessage());
+            }
+            if (!nestedNum.Equals(null))
+            {
+                condMessage.Add(GetNestedNumMessage());
+            }
+            if (!executeNum.Equals(null))
+            {
+                condMessage.Add(GetExecuteNumMessage());
+            }
+            if (!whereCondNum.Equals(null))
+            {
+                condMessage.Add(GetWhereCondNumMessage());
+            }
+
+            return condMessage.ToArray();
+        }
+
+        public int GetConditionNum()
+        {
+            int condNum = 1;  // Init number of condition with 1 because correctness query must have in every puzzle.  
+            if (!joinNum.Equals(null))
+            {
+                condNum += 1;
+            }
+            if (!haveJoin.Equals(null))
+            {
+                condNum += 1;
+            }
+            if (!nestedNum.Equals(null))
+            {
+                condNum += 1;
+            }
+            if (!executeNum.Equals(null))
+            {
+                condNum += 1;
+            }
+            if (!whereCondNum.Equals(null))
+            {
+                condNum += 1;
+            }
+
+            return condNum;
+        }
+
+        private string GetjoinNumMessage()
+        {
+            return "Use JOIN command less than " + joinNum + " time";
+        }
+
+        private string GetHaveJoinMessage()
+        {
+            if (Convert.ToBoolean(haveJoin))
+            {
+                return "Use JOIN command";
+            }
+            else
+            {
+                return "Can't use JOIN command";
+            }
+        }
+
+        private string GetNestedNumMessage()
+        {
+            return "Use nested query less than " + nestedNum + " time";
+        }
+
+        private string GetExecuteNumMessage()
+        {
+            return "Execute query less than " + executeNum + " time";
+        }
+
+        private string GetWhereCondNumMessage()
+        {
+            return "Use condition in WHERE command less than " + whereCondNum + " time";
+        }
     }
 }
