e19f67940c4aefe96fddd9792338183634688edf
Oliver Tongrak <oliver.tongrak@gmail.com>
2023-05-25 22:22:15 +0700

    GameHelper: StringHelper added and documented
    with SceneLoadingHelper documented

--- SQL game build01/Assets/Scripts/SceneLoadingHelper.cs
+++ SQL game build01/Assets/Scripts/SceneLoadingHelper.cs
@@ -8,43 +8,61 @@ namespace GameHelper
 {
     public class SceneLoadingHelper : MonoBehaviour
     {
-        //dynamic var
+        //Dynamic fields
         private Scene _sceneToUnload;
-
+        //marked loaded scene and its action
         private string _sceneNameToCheckOnLoad;
         private Action _actOnCheckOnLoad;
-
+        //marked unloaded scene and its action
         private string _sceneNameToCheckOnUnload;
         private Action _actOnCheckOnUnload;
-
+        //loaded and unloaded mark and on swap action.
         private bool _isSceneLoaded;
         private bool _isSceneUnloaded;
         private Action _actOnSwap;
 
+        #region Unload scene
         public void UnloadScene(Scene scene)
         {
             _sceneToUnload = scene;
             if (_sceneToUnload.IsValid()) SceneManager.sceneLoaded += ActivatorAndUnloader;
             else throw new System.Exception("Scene: " + scene.name + "cann't be unloaded; Due to scene is invalid");
         }
-
+        /// <summary>
+        /// Unload given scene name asyncially
+        /// </summary>
+        /// <param name="scene">name of the scene to be unloaded</param>
         public void UnloadScene(string scene) 
         {
             UnloadScene(SceneManager.GetSceneByName(scene));
         }
-
-
-        public void LoadSceneAdditively(Scene scene)
+        private void ActOnSceneUnloaded(Scene s)
         {
-            LoadSceneAdditively(scene.name);
+            if (s.name.Equals(_sceneNameToCheckOnUnload))
+            {
+                SceneManager.sceneUnloaded -= ActOnSceneUnloaded;
+                _actOnCheckOnUnload();
+            }
         }
+        #endregion
 
+        #region Load scene
+        /// <summary>
+        /// Load scene of given name asycially
+        /// </summary>
+        /// <param name="scene">name of the name to be loaded</param>
+        /// <exception cref="System.Exception"></exception>
         public void LoadSceneAdditively(string scene)
         {
             if (!SceneManager.GetSceneByName(scene).IsValid()) SceneManager.LoadSceneAsync(scene, LoadSceneMode.Additive);
-            else throw new System.Exception("Scene: " + scene + "already be loaded;");
+            else throw new System.Exception(string.Format("Scene({0}) already be loaded;", scene));
         }
-
+        /// <summary>
+        /// Unload and load scene accordingly from the given name
+        /// </summary>
+        /// <param name="toUnload">name of the scene to be unloaded</param>
+        /// <param name="toLoad">name of the scene to be loaded</param>
+        /// <exception cref="System.Exception">threw due to compication</exception>
         public void SwapScene(string toUnload, string toLoad)
         {
             try
@@ -54,20 +72,18 @@ namespace GameHelper
             }
             catch (System.Exception ex)
             {
-                Debug.LogWarning("Cann't swap scenes due to " + ex.Message);
+                throw new System.Exception(string.Format("Cann't swap scenes due to: {0}", ex.Message));
             }
         }
-
+        /// <summary>
+        /// Enfroce given action if given scene loaded
+        /// </summary>
+        /// <param name="scene">name of the scene to check on loaded</param>
+        /// <param name="onComplete">action to act</param>
         public void ActOnSceneLoaded(string scene, Action onComplete)
         {
             ActOnSceneEvent(true, scene, onComplete);
         }
-
-        /*public void ActOnSceneUnloaded(string scene, Action onComplete)
-        {
-            ActOnSceneEvent(false, scene, onComplete);
-        }*/
-
         private void ActOnSceneEvent(bool onLoad, string scene, Action onComplete)
         {
             if (onLoad)
@@ -91,30 +107,32 @@ namespace GameHelper
                 _actOnCheckOnLoad();
             }
         }
-        private void ActOnSceneUnloaded(Scene s)
-        {
-            if (s.name.Equals(_sceneNameToCheckOnUnload))
-            {
-                SceneManager.sceneUnloaded -= ActOnSceneUnloaded;
-                _actOnCheckOnUnload();
-            }
-        }
+        /// <summary>
+        /// Enfroce given action if given scenes detect loaded and unloaded respectivelly
+        /// </summary>
+        /// <param name="unloadScene">name of the scene unloaded</param>
+        /// <param name="loadScene">name of the scene loaded</param>
+        /// <param name="onSwap">action to be act</param>
         public void ActOnSceneSwaped(string unloadScene, string loadScene, Action onSwap)
         {
             ActOnSceneEvent(false, unloadScene, () => { this._isSceneUnloaded = true; });
             ActOnSceneEvent(true, loadScene, () => { this._isSceneLoaded = true; });
             this._actOnSwap = onSwap;
         }
+        #endregion
 
+        #region Misc Function
         private void ActivatorAndUnloader(Scene s, LoadSceneMode m)
         {
             SceneManager.sceneLoaded -= ActivatorAndUnloader;
             SceneManager.SetActiveScene(s);
             SceneManager.UnloadSceneAsync(_sceneToUnload);
         }
+        #endregion
 
         private void Update()
         {
+            //if marked scenes detected loaded and unload respectively act as require.
             if(_isSceneLoaded && _isSceneUnloaded)
             {
                 this._actOnSwap();
