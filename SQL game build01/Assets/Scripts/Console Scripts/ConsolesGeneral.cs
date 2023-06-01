56fa13f3ab5100df49413d85399742807d1834b0
Oliver Tongrak <oliver.tongrak@gmail.com>
2023-06-01 18:43:06 +0700

    Consoles: Base class and Interface added

--- SQL game build01/Assets/Scripts/Console Scripts/ConsolesGeneral.cs
+++ SQL game build01/Assets/Scripts/Console Scripts/ConsolesGeneral.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 namespace ConsoleGeneral
 {
+    //direct delegate
     public delegate void ExcuteButtonHandler(string playerInput);
 
     public enum ConsoleMode { ExploreMode, PuzzleMode, DialogMode}
@@ -12,32 +13,52 @@ namespace ConsoleGeneral
     #region Console Mode Unit
     public static class CMStarterFactory
     {
-        public static CMStarterUnit CreateDialogMode(DialogConsoleController dialogConsole, string[] dialogs, string confirmMessage)
+        public static CMStarterUnit CreateExploreMode() => throw new NotImplementedException();
+        public static CMStarterUnit CreateDialogMode(DialogModeController dialogConsole, string[] dialogs, string confirmMessage)
                 => new DialogModeStarter(dialogConsole, dialogs, confirmMessage);
-
-        public static CMStarterUnit CreatePuzzleMode(PuzzleConsoleMaster puzzleConsole)
-               => new PuzzleConsoleStarter(puzzleConsole);
+        public static CMStarterUnit CreatePuzzleMode(PuzzleModeController puzzleConsole, ExcuteButtonHandler exeHandler)
+               => new PuzzleModeStarter(puzzleConsole, exeHandler);
     }
 
     public interface CMStarterUnit
     {
-        public void StartConsole();
+        public ConsoleMode mode { get; }
+        public void StartUnit(EventHandler modeChangesHandler);
+        public void StopUnit(EventHandler modeChangesHandler);
     }
     #endregion
 
-    public class SingletonClass<T> where T : class, new() 
+    #region Consoles abstracts & interfaces
+    public abstract class ConsoleBasic : UIElementBasic
     {
-        private static T _instance;
-        public static T Instance
-        {
-            get
-            {
-                if (_instance == null) _instance = new T();
-                return _instance;
-            }
-        }
+        public abstract void ShowConsole();
     }
 
+    /*public class CModeChangesArgs : EventArgs
+    {
+        public int CMode { get; }
+    }
+    public class PuzzleModeArgs : CModeChangesArgs
+    {
+        public string playerRespond { get; }
+    }*/
+
+    public abstract class ConsoleModeController : MonoBehaviour
+    {
+        public event EventHandler ConsoleModeChanged;
+        protected void RaiseModeChange(System.Object sender) => 
+            this.ConsoleModeChanged?.Invoke(sender, EventArgs.Empty);
+
+        public void SubToCMChangeEvent(EventHandler handler)
+            => this.ConsoleModeChanged += handler;
+
+        public abstract void InitMode();
+        public abstract void ShowMode();
+        public abstract void HideMode();
+    }
+    #endregion
+
+    #region Consoles element classes
     public class UIElementBasic : MonoBehaviour
     {
         public bool isShow
@@ -46,29 +67,7 @@ namespace ConsoleGeneral
             set => this.gameObject.SetActive(value);
         }
     }
-
-    #region Consoles abstracts & interfaces
-    public abstract class ConsoleBasic : UIElementBasic
-    {
-        public abstract void ShowConsole();
-    }
-
-    public class ConsoleModeController
-    {
-        public event EventHandler ConsoleModeChanged;
-        protected void RaiseModeChanged(System.Object sender)
-        {
-            this.ConsoleModeChanged?.Invoke(sender, EventArgs.Empty);
-        }
-
-        public virtual void InitMode() { }
-        public virtual void ShowMode() { }
-        public virtual void HideMode() { }
-    }
-    #endregion
-
-    #region Consoles element classes
-    public class TextBox : MonoBehaviour
+    public class TextBox : UIElementBasic
     {
         private string _displayText;
         public string text { get { return _displayText; } }
