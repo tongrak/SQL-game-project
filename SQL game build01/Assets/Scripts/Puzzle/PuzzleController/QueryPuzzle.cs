4ff9d1d9a8827b66be3cda48abc3127b59e82aa9
salaza <gun159@outlook.com>
2023-07-28 22:05:14 +0700

    Implement query puzzle component

---
+++ SQL game build01/Assets/Scripts/Puzzle/PuzzleController/QueryPuzzle.cs
@@ -0,0 +1,95 @@
+﻿using Assets.Scripts.Puzzle.PuzzleController.Interface;
+using Puzzle;
+using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Puzzle.PuzzleController
+{
+    public class QueryPuzzle : MonoBehaviour, IQueryPuzzle
+    {
+        [SerializeField] protected TextAsset puzzleFile;
+        [SerializeField] protected DatabaseChapter databaseChapter;
+        [SerializeField] protected QueryPuzzleScoreManager queryPScoreManager;
+
+        private string DBPath { get; set; }
+        private string AnswerQuery { get; set; }
+        private Condition Condition { get; set; }
+        public string[] Dialog { get; private set; }
+        public string Question { get; private set; }
+        public string[] ConditionMessage { get; private set; }
+        public int CurrScore { get; private set; } = 0;
+        public int ExecutedNum { get; private set; } = 0;
+        public PuzzleResult BestPuzzleResult { get; private set; }
+
+        public PuzzleResult AnswerPuzzle(string playerQuery)
+        {
+            ExecutedNum += 1;
+            PuzzleResult latestPuzzleResult = PuzzleEvaluator.GetInstance().EvaluateQuery(DBPath, AnswerQuery, playerQuery, Condition, ExecutedNum);
+            UpdateCurrPResultAndScore(latestPuzzleResult);
+            return latestPuzzleResult;
+        }
+
+        public void ResetExecutedNum()
+        {
+            ExecutedNum = 0;
+        }
+
+        private void UpdateCurrPResultAndScore(PuzzleResult latestPuzzleResult)
+        {
+            int latestScore = PuzzleEvaluator.GetInstance().CalculateQueryScore(latestPuzzleResult.conditionResult);
+            if (latestScore > CurrScore)
+            {
+                // Update current PuzzleResult
+                BestPuzzleResult = latestPuzzleResult;
+
+                // Update puzzle score and total score in manager
+                queryPScoreManager.AddScore(latestScore - CurrScore);
+                CurrScore = latestScore;
+            }
+        }
+
+        #region For awake method
+        // Load puzzle value from json file
+        private void Load_QueryPuzzle()
+        {
+            QueryPuzzleModel puzzle = JsonUtility.FromJson<QueryPuzzleModel>(puzzleFile.text);
+
+            Dialog = puzzle.dialog;
+            Question = puzzle.question;
+            AnswerQuery = puzzle.answer;
+
+            Condition = puzzle.condition;
+            ConditionMessage = Condition.GetConditionMessage();
+
+            BestPuzzleResult = new PuzzleResult(puzzle.condition);
+
+            ResetExecutedNum();
+
+            // locate used database path
+            DBPath = DatabaseFilePath.LocateDBPath(databaseChapter);
+
+            // validate answer query
+            SQLValidator validator = SQLValidator.GetInstance();
+            validator.validatePathAndQuery(DBPath, AnswerQuery);
+        }
+        #endregion
+
+        void Awake()
+        {
+            Load_QueryPuzzle();
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
