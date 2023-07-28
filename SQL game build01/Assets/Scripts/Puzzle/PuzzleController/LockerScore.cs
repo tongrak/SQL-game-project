0f85df6aa182e794598e4e043903ddc29b49f7cd
salaza <gun159@outlook.com>
2023-07-28 22:35:47 +0700

    Implement LockerScore component

---
+++ SQL game build01/Assets/Scripts/Puzzle/PuzzleController/LockerScore.cs
@@ -0,0 +1,24 @@
+﻿using Assets.Scripts.Puzzle.PuzzleController.Interface;
+using UnityEngine;
+
+namespace Assets.Scripts.Puzzle.PuzzleController
+{
+    public class LockerScore : MonoBehaviour, ILockerScore
+    {
+        [field: SerializeField] public int ScoreUse { get; private set; }
+
+        public bool IsLocked { get; private set; } = true;
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
