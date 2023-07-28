27bfb9305091421b897659f93f513a1230baad3d
salaza <gun159@outlook.com>
2023-07-28 22:39:39 +0700

    Implement HaveItem component

---
+++ SQL game build01/Assets/Scripts/Puzzle/PuzzleController/Interface/HaveItem.cs
@@ -0,0 +1,28 @@
+﻿using Puzzle;
+using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Puzzle.PuzzleController.Interface
+{
+    public class HaveItem : MonoBehaviour, IHaveItem
+    {
+        [SerializeField] private KeyItem keyItemCarried;
+
+        public KeyItem GetKeyItem()
+        {
+            return keyItemCarried;
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
